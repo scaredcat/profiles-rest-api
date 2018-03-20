43ec51d6287ccb29998298c9aaeae86d9679f2ee
scaredcat <yuna03@gmail.com>
2018-03-20 17:32:49 +1100

    Mid work

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
+from . import models
 
 class HelloSerializer(serializers.Serializer):
   """Serializesrs a name field for testing our APIView."""
 
   name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.modelSerializer):
+  """A serializer for our user profile objects."""
+
   
